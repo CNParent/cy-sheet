696abc6e08a363cb59556d2d8a92ffac7fd78e6c
Chris Parent <chrisnparent@gmail.com>
2023-01-22 15:24:50 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import character from "./models/character.js"
+
+	import { theme } from './lib/styles.js'
+
+	let model = character();
+</script>
+
+<svelte:head>
+	{#if theme == 'dark'}
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/vinorodrigues/bootstrap-dark@0.6.1/dist/bootstrap-dark.min.css">
+	{:else}
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+	{/if}
+</svelte:head>
+
+<main id="app">
+	Content goes here
+</main>
---
+++ src/models/character.js
@@ -0,0 +1,28 @@
+const character = () => ({
+    name: 'New character',
+    glitches: {
+        die: 'd2',
+        current: 0
+    },
+    hp: {
+        current: 0,
+        max: 0
+    },
+    abilities: {
+        strength: 0,
+        agility: 0,
+        presence: 0,
+        toughness: 0,
+        knowledge: 0
+    },
+    creds: 0,
+    debt: 0,
+    class: '',
+    nano: [],
+    infestations: [],
+    apps: [],
+    armor: '-',
+    weapons: [],
+    equipment: [],
+    cybertech: []
+});
